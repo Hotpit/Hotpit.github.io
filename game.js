5ff65bb967f45cd6585f771d3a54455fc7be8119
Hopit <120403874+Hotpit@users.noreply.github.com>
2025-09-03 14:14:46 -0700

    add if then

--- game.js
+++ game.js
@@ -1,3 +1,12 @@
 alert("Game loaded!");
 // Your game code goes here
 const myImage = document.querySelector("test.png");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+});
